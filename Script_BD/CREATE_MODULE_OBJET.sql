94d1d6c52302020d1727f24473d27b643b15ecf6
katorock <leohkato@gmail.com>
2017-09-07 17:14:23 -0300

    Update CREATE_MODULE_OBJET.sql

--- Script_BD/CREATE_MODULE_OBJET.sql
+++ Script_BD/CREATE_MODULE_OBJET.sql
@@ -1,20 +1,20 @@
 create table Placed_Object_Individual_Spec(
-spec_id	Integer,
+spec_id	Integer PRIMARY KEY,
 geom	Point
 );
 
 create table Placed_Object_Building_Spec(
-spec_id	Integer,
+spec_id	Integer PRIMARY KEY,
 geom	MultiPolygon
 );
 
 create table Placed_Object_Overhead_Wire_Spec(
-spec_id	Integer,
+spec_id	Integer PRIMARY KEY,
 geom	MultiLine
 );
 
 
-create table Placed_Object(
+create table tbl_Placed_Object(
 object_id	Integer,
 name	varchar(1000),
 type	varchar(1000),
@@ -23,21 +23,20 @@ verified	Boolean,
 location_id	Integer,
 region_id	Integer,
 owner_id	Integer,
-height_agl	Numeric,
-height_amls	Numeric,
+height_agl	Number,
+height_amls	Number,
 temporary	Boolean,
 lighting	varchar(1000),
 mark_indicator	varchar(1000),
-type_individual_id	Integer,
-type_building_id	Integer,
-type_overhead_wire_id	Integer
+type_individual_id	Integer REFERENCES Placed_Object_Individual_Spec(spec_id),
+type_building_id	Integer REFERENCES Placed_Object_Building_Spec(spec_id),
+type_overhead_wire_id	Integer REFERENCES Placed_Object_Overhead_Wire_Spec(spec_id)
 );
 
 
-
 create table Placed_Object_Owner(
-owner_id	Integer,
-object_id	Integer,
+owner_id	Integer PRIMARY KEY,
+object_id	Integer REFERENCE tbl_Placed_Object(object_id),
 name	Varchar(1000),
 address	Varchar(1000),
 email	Varchar(1000),
