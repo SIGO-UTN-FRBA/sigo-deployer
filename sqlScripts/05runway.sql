95a463b347f67f2ceb51c4df5c12c59a6131eb07
Leonardo <leohkato@gmail.com>
2017-10-01 23:07:34 -0300

    add runway.sql runway_direction.sql

---
+++ sqlScripts/05runway.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.tbl_runway(
+            runway_id, geom, length, width, airport_id, surface_id)
+    VALUES (135, ST_GeomFromText('LINESTRING (-59.22986388888888 -37.24582222222219, -59.227311111111106 -37.22293333333335)
+',4326), 2550, 48, 1, 1);
+
---
+++ sqlScripts/06runway_direction.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.tbl_runway_directions(
+            direction_id, available, code, geom, runway_id)
+    VALUES (640, TRUE, null, ST_GeomFromText('POINT (-59.22986388888888 -37.24582222222219)',4326),135);
+
+
+
+
