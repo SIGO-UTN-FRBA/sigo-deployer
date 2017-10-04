db0d1c6d62304d132ae0333e2186f7633c30640d
sergio087 <sergio.087@gmail.com>
2017-10-03 21:23:53 -0300

    Fix runway direction import

--- sqlScripts/06runway_direction.sql
+++ sqlScripts/06runway_direction.sql
@@ -1,6 +1,6 @@
 INSERT INTO public.tbl_runway_directions(
-            direction_id, available, code, geom, runway_id)
-    VALUES (640, TRUE, null, ST_GeomFromText('POINT (-59.22986388888888 -37.24582222222219)',4326),135);
+            direction_id, number, position, geom, runway_id)
+    VALUES (640, 19, 0, ST_GeomFromText('POINT (-59.22986388888888 -37.24582222222219)',4326),135);
 
 
 
