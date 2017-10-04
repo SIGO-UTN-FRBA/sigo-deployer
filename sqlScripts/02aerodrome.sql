b5cfa17360ffd24be7fa8beca91673e071f7606c
Leonardo <leohkato@gmail.com>
2017-10-03 21:39:34 -0300

    inverti las coordenadas

--- sqlScripts/02aerodrome.sql
+++ sqlScripts/02aerodrome.sql
@@ -1,6 +1,6 @@
 INSERT INTO public.tbl_aerodromes(
             aerodrome_id, code_fir, code_iata, geom, name_fir, region_id)
-    VALUES (1, 'SABE','AEP',ST_SetSRID(ST_MakePoint(-34.55888888888889, -58.41638888888889), 4326) ,'AEROPARQUE J. NEWBERY (CABA)' , 2);
+    VALUES (1, 'SABE','AEP',ST_SetSRID(ST_MakePoint(-58.41638888888889,-34.55888888888889), 4326) ,'AEROPARQUE J. NEWBERY (CABA)' , 2);
 
 
 
