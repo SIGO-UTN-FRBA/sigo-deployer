9dbf2bcaada60be0a57b8a6cfffae43a5b09d80a
Leonardo <leohkato@gmail.com>
2017-10-07 12:34:20 -0300

    se agrego campo regulacion en aerodromos, se agregaron political location

---
+++ sqlScripts/12political_location_type.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.tbl_political_location_types(type_id, index, name)
+VALUES (1, 1,'Pais');
+
+INSERT INTO public.tbl_political_location_types(type_id, index, name)
+VALUES (2, 2,'Provincia');
+
+INSERT INTO public.tbl_political_location_types(type_id, index, name)
+VALUES (3, 3,'Departamento');
