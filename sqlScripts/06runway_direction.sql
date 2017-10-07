c1b5cfb2c51e9839cb542b46487fbfce0d108ab4
Leonardo <leohkato@gmail.com>
2017-10-07 13:23:32 -0300

    se cambio linestring por polygon en runway

--- sqlScripts/06runway_direction.sql
+++ sqlScripts/06runway_direction.sql
@@ -1,5 +1,5 @@
 INSERT INTO public.tbl_runway_directions(direction_id, number, position, geom, runway_id)
-VALUES (640, 1, 0, ST_GeomFromText('POINT (-59.22986388888888 -37.24582222222219)',4326),135);
+VALUES (640, 1, 0, ST_GeomFromText('POINT (-59.22986388888888 -37.24582222222219)',4326),5);
 
 
 INSERT INTO public.tbl_runway_directions(direction_id, number, position, geom, runway_id)
