f4977aaba89a1cc0e5f16c8e38234bca80fb5bbf
Leonardo <leohkato@gmail.com>
2017-10-09 19:27:05 -0300

    modificacion de runway_direction, modificacion de emular aproach y takeoff

--- sqlScripts/06runway_direction.sql
+++ sqlScripts/06runway_direction.sql
@@ -1,5 +1,5 @@
 INSERT INTO public.tbl_runway_directions(direction_id, number, position, geom, runway_id)
-VALUES (640, 1, 0, ST_GeomFromText('POINT (-59.22986388888888 -37.24582222222219)',4326),5);
+VALUES (12, 1, 0, ST_GeomFromText('POINT (-58.425248 -34.553943)',4326),5);
 
 
 INSERT INTO public.tbl_runway_directions(direction_id, number, position, geom, runway_id)
--- sqlScripts/14Aproach_takeoff.sql
+++ sqlScripts/14Aproach_takeoff.sql
@@ -24,9 +24,9 @@ INSERT INTO public.tbl_runway_approach_sections(section_id, enabled, threshold_e
 
 -----------------------------------------
 INSERT INTO public.tbl_runway_approach_sections(section_id, enabled, threshold_elevation, threshold_length, direction_id)
-    VALUES (nextval('runway_approach_section_sequence'), TRUE, 0, 0,640 );/*AEP*/ 
+    VALUES (nextval('runway_approach_section_sequence'), TRUE, 0, 0,12);/*AEP*/ 
 
     
 INSERT INTO public.tbl_runway_takeoff_sections(section_id, clearway_length, clearway_width, enabled, stopway_length, direction_id)
-    VALUES (nextval('runway_takeoff_section_sequence'), 0, 40, TRUE, 0,640);/*AEP*/    
+    VALUES (nextval('runway_takeoff_section_sequence'), 0, 40, TRUE, 0,12);/*AEP*/    
     
