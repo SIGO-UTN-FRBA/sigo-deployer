af4b9a858a5db2e078ea0a9ff91fc5837abe1825
Leonardo <leohkato@gmail.com>
2017-10-09 20:04:16 -0300

    add runway direction 17/35

--- sqlScripts/06runway_direction.sql
+++ sqlScripts/06runway_direction.sql
@@ -11,6 +11,15 @@ VALUES (10, 11, 0, ST_GeomFromText('POINT (-58.553454 -34.819056)',4326),179);
 
 
 INSERT INTO public.tbl_runway_directions(direction_id, number, position, geom, runway_id)
+VALUES (7, 35, 0, ST_GeomFromText('POINT (-58.525499 -34.832598)',4326),180);
+
+
+INSERT INTO public.tbl_runway_directions(direction_id, number, position, geom, runway_id)
+VALUES (8, 17, 0, ST_GeomFromText('POINT (-58.533951 -34.808264)',4326),180);
+
+
+
+INSERT INTO public.tbl_runway_directions(direction_id, number, position, geom, runway_id)
 VALUES (75, 31, 0, ST_GeomFromText('POINT (-58.553454 -34.819056)',4326),492);
 
 INSERT INTO public.tbl_runway_directions(direction_id, number, position, geom, runway_id)
