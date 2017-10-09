871c838b4d4c892296e29c937c8b7e0357b30b8d
Leonardo <leohkato@gmail.com>
2017-10-09 15:35:20 -0300

    add Aproach and takeoff for emulate direction

---
+++ sqlScripts/14Aproach_takeoff.sql
@@ -0,0 +1,32 @@
+INSERT INTO public.tbl_runway_takeoff_sections(section_id, clearway_length, clearway_width, enabled, stopway_length, direction_id)
+    VALUES (nextval('runway_takeoff_section_sequence'), 0, 45, TRUE, 0,75);/*mardel plata*/
+
+INSERT INTO public.tbl_runway_takeoff_sections(section_id, clearway_length, clearway_width, enabled, stopway_length, direction_id)
+    VALUES (nextval('runway_takeoff_section_sequence'), 0, 45, TRUE, 0,76);/*mardel plata*/    
+    
+INSERT INTO public.tbl_runway_approach_sections(section_id, enabled, threshold_elevation, threshold_length, direction_id)
+    VALUES (nextval('runway_approach_section_sequence'), TRUE, 0, 0, 75);/*mardel plata*/   
+
+INSERT INTO public.tbl_runway_approach_sections(section_id, enabled, threshold_elevation, threshold_length, direction_id)
+    VALUES (nextval('runway_approach_section_sequence'), TRUE, 0, 0, 76);/*mardel plata*/   
+----------------------------------------------------------------------------
+INSERT INTO public.tbl_runway_takeoff_sections(section_id, clearway_length, clearway_width, enabled, stopway_length, direction_id)
+    VALUES (nextval('runway_takeoff_section_sequence'), 0, 60, TRUE, 0,9);/*eze*/
+
+INSERT INTO public.tbl_runway_takeoff_sections(section_id, clearway_length, clearway_width, enabled, stopway_length, direction_id)
+    VALUES (nextval('runway_takeoff_section_sequence'), 0, 45, TRUE, 0,10);/*eze*/    
+    
+INSERT INTO public.tbl_runway_approach_sections(section_id, enabled, threshold_elevation, threshold_length, direction_id)
+    VALUES (nextval('runway_approach_section_sequence'), TRUE, 0, 0, 9);/*eze*/ 
+
+INSERT INTO public.tbl_runway_approach_sections(section_id, enabled, threshold_elevation, threshold_length, direction_id)
+    VALUES (nextval('runway_approach_section_sequence'), TRUE, 0, 0, 10);/*eze*/ 
+
+-----------------------------------------
+INSERT INTO public.tbl_runway_approach_sections(section_id, enabled, threshold_elevation, threshold_length, direction_id)
+    VALUES (nextval('runway_approach_section_sequence'), TRUE, 0, 0,640 );/*AEP*/ 
+
+    
+INSERT INTO public.tbl_runway_takeoff_sections(section_id, clearway_length, clearway_width, enabled, stopway_length, direction_id)
+    VALUES (nextval('runway_takeoff_section_sequence'), 0, 40, TRUE, 0,640);/*AEP*/    
+    
