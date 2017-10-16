92f707616c9e0d59cd86fe1fcb0bebf736051b25
Leonardo <leohkato@gmail.com>
2017-10-16 10:57:06 -0300

    se agrego elevacion en el approach

--- sqlScripts/15aproach.sql
+++ sqlScripts/15aproach.sql
@@ -1,4 +1,1 @@
-INSERT INTO public.tbl_runway_approach_sections
-select nextval('runway_approach_section_sequence') ,TRUE,0,0,rd.direction_id
-from tbl_runway_directions rd,tbl_runway r
-where rd.runway_id=r.runway_id;
+copy tbl_runway_approach_sections from '/tmp/approach.csv' USING delimiters ';' ;
