ac346f987b4f3cec6426e0b639fa6f8a53d8befc
sergio087 <sergio.087@gmail.com>
2017-10-16 00:30:43 -0300

    Fix azimuth value

--- sqlScripts/06runway_direction.sql
+++ sqlScripts/06runway_direction.sql
@@ -1,4 +1,4 @@
 copy tbl_runway_directions (direction_id, geom,number, position,runway_id) from '/tmp/runway_direction.csv' USING delimiters ',' ;
 
 
-update tbl_runway_directions set number=number *10;
+update tbl_runway_directions set azimuth=number *10;
