0b9acfbbfade1a348c9cbc05b1495b1f200047e0
Leonardo <leohkato@gmail.com>
2017-10-20 19:25:02 -0300

    modificacion de departamento en objeto

--- sqlScripts/19objects.sql
+++ sqlScripts/19objects.sql
@@ -3,7 +3,7 @@ VALUES (1, 'zarraga 3865', 'Leonardo@gmail.com', 'leonardo', '123456', '123566')
 
 
 INSERT INTO public.tbl_placed_object(object_id, height_agl, height_amls, lighting, mark_indicator,name, subtype, temporary, type, verified, owner_id, location_id)
-VALUES (1, 613.61, 634, 2, 1, 'Torre de iluminacion', 'Torre de iluminacion', FALSE, 1,TRUE,  1, 151);
+VALUES (1, 613.61, 634, 2, 1, 'Torre de iluminacion', 'Torre de iluminacion', FALSE, 1,TRUE,  1, 251);
 
 
 INSERT INTO public.tbl_placed_object_individual_spec(spec_id,geom,object_id)
@@ -11,14 +11,14 @@ VALUES (1,ST_GeomFromText('POINT (-68.42253405735107 -31.571135273247418)',4326)
 
 
 INSERT INTO public.tbl_placed_object(object_id, height_agl, height_amls, lighting, mark_indicator,name, subtype, temporary, type, verified, owner_id, location_id)
-VALUES (2, 621.52, 641.85, 0, 0, 'Antena', 'Antena', FALSE, 1,TRUE, 1, 151);
+VALUES (2, 621.52, 641.85, 0, 0, 'Antena', 'Antena', FALSE, 1,TRUE, 1, 251);
 
 INSERT INTO public.tbl_placed_object_individual_spec(spec_id,geom,object_id)
 VALUES (2,ST_GeomFromText('POINT (-68.42254220236559 -31.571669199516123)',4326),2);
 
 
 INSERT INTO public.tbl_placed_object(object_id, height_agl, height_amls, lighting, mark_indicator,name, subtype, temporary, type, verified,  owner_id, location_id)
-VALUES (3, 608.5, 624.4, 9, 5, 'Arbol individual', 'Arbol', FALSE, 1,TRUE, 1, 151);
+VALUES (3, 608.5, 624.4, 9, 5, 'Arbol individual', 'Arbol', FALSE, 1,TRUE, 1, 251);
 
 INSERT INTO public.tbl_placed_object_individual_spec(spec_id,geom,object_id)
 VALUES (3,ST_GeomFromText('POINT (-68.42236363486106 -31.57422520784287)',4326),3);
