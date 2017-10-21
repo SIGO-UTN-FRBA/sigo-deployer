2be4f71148f87e95864d7d314e7cbd0b187c004d
Leonardo <leohkato@gmail.com>
2017-10-21 10:46:14 -0300

    sea agregaron objetos de ezeiza individual, building

--- sqlScripts/11ols_rules_ICAOAnnex14.sql
+++ sqlScripts/11ols_rules_ICAOAnnex14.sql
@@ -1,1 +1,1 @@
-copy tbl_OLS_rules_ICAOAnnex14 from '/tmp/OLS_Rules_ICAOAnnex14.csv' USING delimiters '|' ;
+copy tbl_OLS_rules_ICAOAnnex14(rule_id, surface_name,runway_classification , runway_category, runway_code_number, property, value) from '/tmp/OLS_Rules_ICAOAnnex14.csv' USING delimiters ',' ;
--- sqlScripts/19objects.sql
+++ sqlScripts/19objects.sql
@@ -25,6 +25,18 @@ VALUES (3,ST_GeomFromText('POINT (-68.42236363486106 -31.57422520784287)',4326),
 
 
 INSERT INTO public.tbl_placed_object(object_id, height_agl, height_amls, lighting, mark_indicator,name, subtype, temporary, type, verified,  owner_id, location_id)
+VALUES (7, 20, 30, 9, 5, 'Puesto de Control', 'Puesto de Control', FALSE, 1,TRUE, 1, 101);
+
+INSERT INTO public.tbl_placed_object_individual_spec(spec_id,geom,object_id)
+VALUES (4,ST_GeomFromText('POINT (-58.528804779052734 -34.82981121017313)',4326),7);
+
+
+
+
+
+
+
+INSERT INTO public.tbl_placed_object(object_id, height_agl, height_amls, lighting, mark_indicator,name, subtype, temporary, type, verified,  owner_id, location_id)
 VALUES (4, 20, 50, 9, 5, 'Galpon 1', 'Galpon', FALSE, 0,TRUE, 1, 101);
 
 
@@ -42,4 +54,12 @@ VALUES (2,
 ST_GeomFromGeoJSON('{"type":"MultiPolygon","coordinates":[[[[-58.538256883621216,-34.81768777397881],[-58.53860020637512,-34.817969632202775],[-58.538385629653924,-34.81814579310304],[-58.53833198547363,-34.81818983326926],[-58.53817105293274,-34.81804890465451],[-58.53806376457214,-34.81791678385923],[-58.537999391555786,-34.817881551611364],[-58.53812813758851,-34.81777585477738],[-58.538256883621216,-34.81768777397881]]]]}
 '),5);
 
+
+INSERT INTO public.tbl_placed_object(object_id, height_agl, height_amls, lighting, mark_indicator,name, subtype, temporary, type, verified,  owner_id, location_id)
+VALUES (6, 20, 40, 9, 5, 'Galpon 3', 'Galpon', FALSE, 0,TRUE, 1, 101);
+
+
+INSERT INTO public.tbl_placed_object_building_spec(spec_id,geom,object_id)
+VALUES (3,
+ST_GeomFromGeoJSON('{"type": "MultiPolygon","coordinates": [[[[-58.538256883621216,-34.81768777397881],[-58.53860020637512,-34.817969632202775],[-58.538385629653924,-34.81814579310304],[-58.53833198547363,-34.81818983326926],[-58.53817105293274,-34.81804890465451],[-58.53806376457214,-34.81791678385923],[-58.537999391555786,-34.817881551611364],[-58.53812813758851,-34.81777585477738],[-58.538256883621216,-34.81768777397881]]]]}'),6);
 
