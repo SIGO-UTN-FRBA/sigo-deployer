8a8756e2750f3a6bd8ec708618dd3592c1dfbe11
Leonardo <leohkato@gmail.com>
2017-10-21 15:25:41 -0300

    modificacion de nombre del campo runway_code en OLSRULE

--- sqlScripts/11ols_rules_ICAOAnnex14.sql
+++ sqlScripts/11ols_rules_ICAOAnnex14.sql
@@ -1,4 +1,4 @@
-copy tbl_OLS_rules_ICAOAnnex14(rule_id, surface_name,runway_classification , runway_category, runway_code, property, value) from '/tmp/OLS_Rules_ICAOAnnex14.csv' USING delimiters ',' ;
+copy tbl_OLS_rules_ICAOAnnex14(rule_id, surface_name,runway_classification , runway_category, runway_code_number, property, value) from '/tmp/OLS_Rules_ICAOAnnex14.csv' USING delimiters ',' ;
 
 
 
