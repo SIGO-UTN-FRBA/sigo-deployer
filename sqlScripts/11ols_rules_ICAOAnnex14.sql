25e389be2d2d1ed09a0238bdfa6b95500afa37f8
Leonardo <leohkato@gmail.com>
2017-10-22 08:39:16 -0300

    modificacion de ols_rules code_number

--- sqlScripts/11ols_rules_ICAOAnnex14.sql
+++ sqlScripts/11ols_rules_ICAOAnnex14.sql
@@ -1,6 +1,7 @@
-copy tbl_OLS_rules_ICAOAnnex14(rule_id, surface_name,runway_classification , runway_category, runway_code_number, property, value) from '/tmp/OLS_Rules_ICAOAnnex14.csv' USING delimiters ',' ;
+/*copy tbl_OLS_rules_ICAOAnnex14(rule_id, surface_name,runway_classification , runway_category, runway_code_number, property, value) from '/tmp/OLS_Rules_ICAOAnnex14.csv' USING delimiters ',' ;*/
 
 
+copy tbl_OLS_rules_ICAOAnnex14 from '/tmp/OLS_Rules_ICAOAnnex14.csv' USING delimiters '|' ;
 
 INSERT INTO public.tbl_ols_rules(rule_id,regulation_id)
 select rule_id,1
