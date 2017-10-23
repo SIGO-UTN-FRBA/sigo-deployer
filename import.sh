594cee9ced27037ca34c32c55fc8e64589a455bc
Leonardo <leohkato@gmail.com>
2017-10-23 19:16:41 -0300

    modificacion de import

--- import.sh
+++ import.sh
@@ -2,24 +2,16 @@
 
 cd $(dirname $0)
 
-echo "[$(date +%m%d%H%M%S)] Start import.." > resul.log
+echo "[$(date +%m%d%H%M%S)] Start import.." 
 
 cp ./csvFiles/* /tmp
 ls -1 ./sqlScripts/*.sql | while read linea; do
   echo " Procesando $linea"
-  psql sigo < $linea  >> resul.log
+  psql sigo < $linea 
 done
 
 
 rm /tmp/*csv
-
-grep ERROR resul.log
-if [ $? -eq 0 ]
- then
-
-echo "hay errores verificar resul.log"
-
- fi
 
 echo "$(date +%m%d%H%M%S) Fin proceso"
 
