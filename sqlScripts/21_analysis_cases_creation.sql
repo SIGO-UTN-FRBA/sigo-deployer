6cdc3c9b0342b939af6c9f6be3015ffe67e7a6ca
sergio087 <sergio.087@gmail.com>
2017-11-03 21:23:57 -0300

    Fix objets and cases

--- sqlScripts/21_analysis_cases_creation.sql
+++ sqlScripts/21_analysis_cases_creation.sql
@@ -5,6 +5,6 @@
 ALTER SEQUENCE analysis_case_sequence RESTART WITH 1;
   ALTER SEQUENCE analysis_case_sequence INCREMENT BY 1;
 
-INSERT INTO tbl_analysis_cases(case_id, aerodrome_id, status_id)
-  SELECT nextval('analysis_case_sequence'), ta.aerodrome_id, 1
+INSERT INTO tbl_analysis_cases(case_id, aerodrome_id, status_id, stage_id, creation_date)
+  SELECT nextval('analysis_case_sequence'), ta.aerodrome_id, 1, 3, now()
   FROM tbl_aerodromes ta;
