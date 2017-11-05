28b4210c1e88a5578ef5dd8a1b55c868c8f6e497
sergio087 <sergio.087@gmail.com>
2017-11-05 18:37:59 -0300

    Import analysis and analysis cases

--- sqlScripts/21_analysis_cases_creation.sql
+++ sqlScripts/21_analysis_cases_creation.sql
@@ -1,10 +1,9 @@
-/*Inserta el valor para los tipos completados para no perder la integridad referencial*/
 
-/*Inserta un caso para todos los aeropuertos con estado completado*/
-
-ALTER SEQUENCE analysis_case_sequence RESTART WITH 1;
-  ALTER SEQUENCE analysis_case_sequence INCREMENT BY 1;
-
-INSERT INTO tbl_analysis_cases(case_id, aerodrome_id, status_id, stage_id, creation_date)
-  SELECT nextval('analysis_case_sequence'), ta.aerodrome_id, 1, 3, now()
+INSERT INTO tbl_analysis(analysis_id, status_id, stage_id, creation_date)
+  SELECT ta.aerodrome_id, 2, 3, now()
   FROM tbl_aerodromes ta;
+
+INSERT INTO tbl_analysis_cases(case_id, analysis_id, aerodrome_id)
+  SELECT ta.aerodrome_id, ta.aerodrome_id, ta.aerodrome_id
+  FROM tbl_aerodromes ta;
+
