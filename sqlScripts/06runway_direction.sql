d9e1a9c84b01bd2c2763b7908bced333ad9ab022
sergio087 <sergio.087@gmail.com>
2017-11-24 20:46:48 -0300

    Load code letter ICAO for airports

--- sqlScripts/06runway_direction.sql
+++ sqlScripts/06runway_direction.sql
@@ -11,3 +11,14 @@ from tbl_runway_directions;
 INSERT INTO public.tbl_runway_classification_icaoannex14(category, classification, type_letter, type_number, classification_id)
 select null,null,null,null,classification_id from tbl_runway_classification;  
 
+with tmp as (
+  select c.classification_id
+  from tbl_runway_classification as c
+  join tbl_runway_directions as d on d.direction_id = c.direction_id
+  JOIN tbl_runway as r on d.runway_id = r.runway_id
+  join tbl_aerodromes as a on r.airport_id = a.aerodrome_id
+  where code_fir in ('SAZS','SAZB','SANC','SACO','SARC','SAME','SAVY','SAWC','SAWE','SARI','SASJ','SAZM','SAZR','SAAP','SAAR','SATR','SASA','SANE','SAOC','SANT','SAVT','SANR','SAVV')
+)
+UPDATE tbl_runway_classification_icaoannex14
+SET type_letter=1
+WHERE exists(select 1 from tmp t where t.classification_id = tbl_runway_classification_icaoannex14.classification_id);
