c4151adfaa1693c2867f63c537573da459669318
sergio087 <sergio.087@gmail.com>
2017-11-26 17:23:40 -0300

    Set analysis's edition_date property

--- sqlScripts/21_analysis_cases_creation.sql
+++ sqlScripts/21_analysis_cases_creation.sql
@@ -1,5 +1,5 @@
-INSERT INTO tbl_analysis(analysis_id, status_id, stage_id, creation_date)
-  SELECT ta.aerodrome_id, 2, 3, now()
+INSERT INTO tbl_analysis(analysis_id, status_id, stage_id, creation_date, edition_date)
+  SELECT ta.aerodrome_id, 2, 3, now(), now()
   FROM tbl_aerodromes ta;
 
 INSERT INTO tbl_analysis_cases(case_id, analysis_id, aerodrome_id)
