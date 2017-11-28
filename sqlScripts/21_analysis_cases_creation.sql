53a6ffc5d756dd3d778bc1bceafa4b195f608922
sergio087 <sergio.087@gmail.com>
2017-11-28 00:08:58 -0300

    Set regulationId on analysis table

--- sqlScripts/21_analysis_cases_creation.sql
+++ sqlScripts/21_analysis_cases_creation.sql
@@ -1,5 +1,5 @@
-INSERT INTO tbl_analysis(analysis_id, status_id, stage_id, creation_date, edition_date)
-  SELECT ta.aerodrome_id, 2, 3, now(), now()
+INSERT INTO tbl_analysis(analysis_id, status_id, stage_id, creation_date, edition_date, regulation_id)
+  SELECT ta.aerodrome_id, 2, 3, now(), now(), 0
   FROM tbl_aerodromes ta;
 
 INSERT INTO tbl_analysis_cases(case_id, analysis_id, aerodrome_id)
