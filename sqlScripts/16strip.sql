fd08dcf5d9a90acc0d8f8c5e90f342a8168ada92
sergio087 <sergio.087@gmail.com>
2017-12-11 13:48:09 -0300

    Initialize runway direction strip

--- sqlScripts/16strip.sql
+++ sqlScripts/16strip.sql
@@ -1,33 +1,25 @@
---SAEZ 11
-INSERT INTO tbl_runway_strips(strip_id, strip_length, strip_width)
-    VALUES (1,3420,300);
+INSERT INTO tbl_runway_strips(strip_id) SELECT direction_id FROM tbl_runway_directions;
 
 UPDATE tbl_runway_directions
-  SET strip_id = 1
-  WHERE direction_id = 10;
+SET strip_id = direction_id;
+
+--SAEZ 11
+UPDATE tbl_runway_strips
+SET strip_length = 3420, strip_width = 300
+WHERE strip_id = 10;
 
 --SAEZ 17
-INSERT INTO tbl_runway_strips(strip_id, strip_length, strip_width)
-    VALUES (2,3225,300);
-
-UPDATE tbl_runway_directions
-  SET strip_id = 2
-  WHERE direction_id = 8;
+UPDATE tbl_runway_strips
+SET strip_length = 3225, strip_width = 300
+WHERE strip_id = 8;
 
 --SAZM 13
-INSERT INTO tbl_runway_strips(strip_id, strip_length, strip_width)
-    VALUES (3,2320,300);
-
-
-UPDATE tbl_runway_directions
-  SET strip_id = 3
-  WHERE direction_id = 76;
+UPDATE tbl_runway_strips
+SET strip_length = 2320, strip_width = 300
+WHERE strip_id = 76;
 
 --SAZM 31
-INSERT INTO tbl_runway_strips(strip_id, strip_length, strip_width)
-    VALUES (4,2390,300);
+UPDATE tbl_runway_strips
+SET strip_length = 2390, strip_width = 300
+WHERE strip_id = 75;
 
-
-UPDATE tbl_runway_directions
-  SET strip_id = 4
-  WHERE direction_id = 75;
