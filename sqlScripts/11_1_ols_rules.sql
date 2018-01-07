37cf2779482a722901943a5bec4c76e4756cb179
Leonardo <leohkato@gmail.com>
2018-01-07 20:19:38 -0300

    modificacion de ingresar la ols rules ICAOAnnex de csv a sqlademas se puso nullcuando el surfacename es 11

--- sqlScripts/11_1_ols_rules.sql
+++ sqlScripts/11_1_ols_rules.sql
@@ -1,7 +1,7 @@
 
 copy public.tbl_ols_rules(rule_id, regulation_id)
 from '/tmp/OLS_Rules.csv' USING delimiters '|' ;
-
+/*
 copy tbl_ols_rules_icaoannex14 (property_code, property_name, runway_category, runway_classification, runway_code_number, surface_name, value, rule_id)
 from '/tmp/OLS_Rules_ICAOAnnex14.csv' USING delimiters ',' ;
-
+*/
