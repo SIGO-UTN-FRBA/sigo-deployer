9df04a57e7537906217b87387672b7a8cee62e36
Leonardo <leohkato@gmail.com>
2018-01-08 17:12:25 -0300

    separe la carga de classification ICAO,ya no hace mas update carga la tabla final

--- sqlScripts/06_1_runway_direction.sql
+++ sqlScripts/06_1_runway_direction.sql
@@ -1000,29 +1000,3 @@ update tbl_runway_directions set azimuth=number *10;
 INSERT INTO public.tbl_runway_classification 
 select nextval('runwayClassificationGenerator_SEQUENCE'),direction_id
 from tbl_runway_directions;
-
-INSERT INTO public.tbl_runway_classification_icaoannex14(category, classification, type_letter, type_number, classification_id)
-select null,null,null,null,classification_id from tbl_runway_classification;  
-
-with tmp as (
-  select c.classification_id
-  from tbl_runway_classification as c
-  join tbl_runway_directions as d on d.direction_id = c.direction_id
-  JOIN tbl_runway as r on d.runway_id = r.runway_id
-  join tbl_aerodromes as a on r.airport_id = a.aerodrome_id
-  where code_fir in ('SAZS','SAZB','SANC','SACO','SARC','SAME','SAVY','SAWC','SAWE','SARI','SASJ','SAZM','SAZR','SAAP','SAAR','SATR','SASA','SANE','SAOC','SANT','SAVT','SANR','SAVV')
-)
-UPDATE tbl_runway_classification_icaoannex14
-SET type_letter=1
-WHERE exists(select 1 from tmp t where t.classification_id = tbl_runway_classification_icaoannex14.classification_id);
-
-
-
-UPDATE tbl_runway_classification_icaoannex14
-SET type_number=4,category=2,classification=2
-WHERE classification_id in (175,176,967,961,962,32,729,991,986,959,960,981,980,179,990,332,333,957,16,93,92,600,601,875,876,939,982,416,938,965,964,178,177,987,988,984,985,888);
-
-
-UPDATE tbl_runway_classification_icaoannex14
-SET type_number=3,category=2,classification=2
-WHERE classification_id in (920,4,979,937);
