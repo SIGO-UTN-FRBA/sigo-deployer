96f1fedf24d07b30f8c9b34f18168b9ad3bb3219
Leonardo <leohkato@gmail.com>
2018-02-09 10:15:36 -0300

    elmiminacion de invocacion del directorio csv ya no existia

--- import.sh
+++ import.sh
@@ -4,14 +4,12 @@ cd $(dirname $0)
 
 echo "[$(date +%m%d%H%M%S)] Start import.." 
 
-cp ./csvFiles/* /tmp
 ls -1 ./sqlScripts/*.sql | while read linea; do
   echo " Procesando $linea"
   psql sigo < $linea 
 done
 
 
-rm -f /tmp/*csv
 
 echo "$(date +%m%d%H%M%S) Fin proceso"
 
