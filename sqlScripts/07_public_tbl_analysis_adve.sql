b6d165fb8d8813ce6d45ec5e31f1c458836a0caf
Sergio Nascimbene <sergio.087@gmail.com>
2018-02-11 01:11:18 -0300

    Import adversed effect aspects and mitigation measures

---
+++ sqlScripts/07_public_tbl_analysis_adve.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.tbl_analysis_adverse_effect_aspects (aspect_id, description, name) VALUES (1, null, 'Aerodrome control service');
+INSERT INTO public.tbl_analysis_adverse_effect_aspects (aspect_id, description, name) VALUES (2, null, 'Physical characteristics of the aerodrome');
+INSERT INTO public.tbl_analysis_adverse_effect_aspects (aspect_id, description, name) VALUES (3, null, 'Air navigation aids');
+INSERT INTO public.tbl_analysis_adverse_effect_aspects (aspect_id, description, name) VALUES (4, null, 'Air operations under normal conditions');
+INSERT INTO public.tbl_analysis_adverse_effect_aspects (aspect_id, description, name) VALUES (5, null, 'Air operations in case of contingency');
+INSERT INTO public.tbl_analysis_adverse_effect_aspects (aspect_id, description, name) VALUES (6, null, 'Flight safety');
--- sqlScripts/22_analysis_result_reasons.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO public.tbl_analysis_result_reasons (reason_id, description, keep, obstacle) VALUES (5, 'Other', true, true);
-INSERT INTO public.tbl_analysis_result_reasons (reason_id, description, keep, obstacle) VALUES (2, 'Frangible', true, true);
-INSERT INTO public.tbl_analysis_result_reasons (reason_id, description, keep, obstacle) VALUES (1, 'Preexisting and fixed', true, true);
-INSERT INTO public.tbl_analysis_result_reasons (reason_id, description, keep, obstacle) VALUES (6, 'Operational safety is compromised', false, true);
-INSERT INTO public.tbl_analysis_result_reasons (reason_id, description, keep, obstacle) VALUES (4, 'Insurmountable obstacle', true, true);
-INSERT INTO public.tbl_analysis_result_reasons (reason_id, description, keep, obstacle) VALUES (3, 'Shadowed', true, true);
-INSERT INTO public.tbl_analysis_result_reasons (reason_id, description, keep, obstacle) VALUES (7, 'Operational safety is not compromised', true, false);
